6a7544a0268013d70bceeaf1489604c42f9fe5b9
DagonR <29314227+gerauf7@users.noreply.github.com>
2018-01-15 17:39:27 +0300

    Add files via upload

---
+++ elliptic_curve.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    a = -3
+    b = 4
+
+    y, x = np.ogrid[-5:5:100j, -5:5:100j]
+    plt.contour(x.ravel(), y.ravel(), pow(y, 2) + x*y - pow(x, 3) - x * a - b, [0])
+    plt.grid()
+    plt.show()
+
+if __name__ == '__main__':
+    main()
